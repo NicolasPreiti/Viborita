7f192c374033e96b8cd278a4447163aec6651282
NicolasP <nicolaspreiti45@gmail.com>
2022-04-13 16:45:10 -0300

    viborita-4

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 let PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res)=>{
     res.send("Accediste a viborita");
