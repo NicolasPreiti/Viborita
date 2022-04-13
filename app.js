e6d81d3ba7c00e3363ec35f79061c7ab59d0a97e
NicolasP <nicolaspreiti45@gmail.com>
2022-04-13 18:14:49 -0300

    viborita-5

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res)=>{
-    res.send("Accediste a viborita");
 })
 
 app.post("/", (req, res)=>{
