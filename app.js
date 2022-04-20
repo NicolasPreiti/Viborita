fbc9a3288c56236bc46380be93cad8788deac585
NicolasP <nicolaspreiti45@gmail.com>
2022-04-20 01:05:34 -0300

    commit-9

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { redirect } = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,11 +6,6 @@ app.use(express.static("./Public"));
 app.use(express.json());
 
 app.get("/", (req, res)=>{
-    res.send("otra vez no se cargo")
-});
-
-app.get("https://www.viborita.herokuapp.com", (req, res)=>{
-    res.redirect("https://viborita.herokuapp.com");
 });
 
 app.post("/", (req, res)=>{
