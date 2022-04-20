729b4b312695c6637bec949c79b4e1801da3fbe9
NicolasP <nicolaspreiti45@gmail.com>
2022-04-20 00:56:30 -0300

    commit-8

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { redirect } = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,14 +8,18 @@ app.use(express.json());
 
 app.get("/", (req, res)=>{
     res.send("otra vez no se cargo")
-})
+});
+
+app.get("https://www.viborita.herokuapp.com", (req, res)=>{
+    res.redirect("https://viborita.herokuapp.com");
+});
 
 app.post("/", (req, res)=>{
     let datos = req.body;
     res.json({
         infoDelBackend: "Respuesta del backend"
     })
-})
+});
 
 app.listen(port, ()=>{
     console.log("Server funcionando")
